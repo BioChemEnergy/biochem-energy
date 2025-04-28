8d32359546c70aa2f5c2d7a3e483f5408dee967d
BioChemEnergy <bce.biochem@gmail.com>
2025-04-28 17:26:56 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,10 +23,10 @@ export default function Home() {
       <div className="w-48 h-36 hover:scale-125 transition-transform ease-in-out duration-500 flex items-center justify-center">
         <Image src="/decontamination.PNG" alt="Air Decontamination" width={180} height={130} />
       </div>
-      <div className="w-48 h-36 hover:scale-125 transition-transform ease-in-out duration-500 flex items-center justify-center">
+      <div className="w-48 h-36 hover:scale-170 transition-transform ease-in-out duration-500 flex items-center justify-center">
         <Image src="/biofuel.png" alt="Biofuel Production" width={180} height={130} />
       </div>
-      <div className="w-48 h-36 hover:scale-125 transition-transform ease-in-out duration-500 flex items-center justify-center">
+      <div className="w-48 h-36 hover:scale-170 transition-transform ease-in-out duration-500 flex items-center justify-center">
         <Image src="/cvrcky.png" alt="Insect Recycling" width={180} height={130} />
       </div>
     </div>
